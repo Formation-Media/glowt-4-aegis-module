8f73767b641d31e19bfcdcaef5ff3ae153495e75
Jason Millis <jas-n@outlook.com>
2020-08-28 17:49:45 +0100

    Hook into HR set-up page

---
+++ Resources/views/hooks/set-up-page.blade.php
@@ -0,0 +1,6 @@
+<x-card title="{{ __('Competency Suppliers') }}">
+    <p>Manage competency suppliers.</p>
+    <x-slot name="footer">
+        <x-link href="/a/m/Aegis/suppliers" style="primary" title="Suppliers"/>
+    </x-slot>
+</x-card>
