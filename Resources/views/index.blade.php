37939091cc4d981baff17e4c850673a9edbc344f
Jason Millis <jas-n@outlook.com>
2020-08-28 17:49:59 +0100

    Delete index.blade.php

--- Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('aegis::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('aegis.name') !!}
-    </p>
-@endsection
