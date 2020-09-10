1d5cbae0070d9318882ea09a97464fcedeec0455
Jas-n <jas-n@outlook.com>
2020-09-10 17:27:21 +0100

    Copy tweak

--- Resources/views/hooks/set-up-page.blade.php
+++ Resources/views/hooks/set-up-page.blade.php
@@ -1,4 +1,4 @@
-<x-card title="{{ __('Competency Companies') }}">
+<x-card title="{{ __('Companies') }}">
     <p>Manage competency companies.</p>
     <x-slot name="footer">
         <x-link href="/a/m/AEGIS/companies" style="primary" title="Companies"/>
