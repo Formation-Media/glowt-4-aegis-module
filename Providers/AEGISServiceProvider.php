a75567cb71b44b2607347ce5be3cb8d826afae0f
Jason Millis <jas-n@outlook.com>
2020-10-16 10:08:20 +0100

    Laravel 8 update

--- Providers/AEGISServiceProvider.php
+++ Providers/AEGISServiceProvider.php
@@ -3,7 +3,7 @@
 namespace Modules\AEGIS\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AEGISServiceProvider extends ServiceProvider
 {
@@ -98,7 +98,7 @@ class AEGISServiceProvider extends ServiceProvider
     public function registerFactories()
     {
         if (! app()->environment('production') && $this->app->runningInConsole()) {
-            app(Factory::class)->load(module_path($this->moduleName, 'Database/factories'));
+            $this->loadFactoriesFrom(module_path($this->moduleName, 'Database/factories'));
         }
     }
 
