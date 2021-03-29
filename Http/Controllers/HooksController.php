791dd070310fe557894dccaea961b173211442fe
Jas-n <Jas-n@users.noreply.github.com>
2021-03-29 10:56:19 +0100

    Bugfixes

--- Http/Controllers/HooksController.php
+++ Http/Controllers/HooksController.php
@@ -53,9 +53,16 @@ class HooksController extends AEGISController
     }
     public static function collect_hr__edit_competency($args){
         if(isset($args['request']->aegis)){
-            CompetencyCompany::where('competency_id',$args['competency']->id)->first()->update([
-                'company_id'=>$args['request']->aegis['company']
-            ]);
+            if($cc=CompetencyCompany::where('competency_id',$args['competency']->id)->first()){
+                $cc->update([
+                    'company_id'=>$args['request']->aegis['company']
+                ]);
+            }else{
+                $competency_company               =new CompetencyCompany;
+                $competency_company->competency_id=$args['competency']->id;
+                $competency_company->company_id   =$args['request']   ->aegis['company'];
+                $competency_company->save();
+            }
         }
     }
     public static function collect_hr__view_add_competency_fields($args){
