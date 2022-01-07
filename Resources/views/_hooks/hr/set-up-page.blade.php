60481323ef581ca5d040a5b754fda34dc6f3d9ba
Jas-n <Jas-n@users.noreply.github.com>
2022-01-07 16:10:12 +0000

    Code Organisation

--- Resources/views/_hooks/hr/set-up-page.blade.php
+++ Resources/views/_hooks/hr/set-up-page.blade.php
@@ -1,5 +1,5 @@
 @if($permissions['index'])
-    <x-card title="{{ __('Companies') }}">
+    <x-card class="shadow" title="{{ __('Companies') }}">
         <p>Manage competency companies.</p>
         <x-slot name="footer">
             <x-link href="/a/m/AEGIS/companies" style="primary" title="Companies"/>
