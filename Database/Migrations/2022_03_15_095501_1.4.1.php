4561998714ba75ce3fbc40db2d46b2188a86f49e
Jas-n <Jas-n@users.noreply.github.com>
2022-03-15 14:44:15 +0000

    Fix migration

--- Database/Migrations/2022_03_15_095501_1.4.1.php
+++ Database/Migrations/2022_03_15_095501_1.4.1.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::rename('m_aegis_competency_details', 'm_aegis_competency_details');
+        Schema::rename('m_aegis_competency_companies', 'm_aegis_competency_details');
         Schema::table('m_aegis_competency_details', function (Blueprint $table){
             $table->text('live_document')->nullable()->after('company_id');
         });
@@ -26,8 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::rename('m_aegis_competency_details', 'm_aegis_competency_details');
-        Schema::table('m_aegis_competency_details', function (Blueprint $table){
+        Schema::rename('m_aegis_competency_details', 'm_aegis_competency_companies');
+        Schema::table('m_aegis_competency_companies', function (Blueprint $table){
             $table->dropColumn('live_document');
         });
     }
