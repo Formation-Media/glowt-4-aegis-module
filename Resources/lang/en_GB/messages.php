43abab0385233656788dcd2ac659acf58cf77209
Jas-n <Jas-n@users.noreply.github.com>
2022-04-13 17:26:14 +0100

    Translation improvements

--- Resources/lang/en_GB/messages.php
+++ Resources/lang/en_GB/messages.php
@@ -1,6 +1,9 @@
 <?php
 
 return array(
+    'customer' => [
+        'reference-message' => 'The Reference is a code used to identify the Customer',
+    ],
     'project' => [
         'reference-exists' => 'A project with this reference already exists.',
     ],
