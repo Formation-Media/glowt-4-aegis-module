4fcc9400ae9431e6f60cf4f2c39b5c6d76c262c3
Jas-n <Jas-n@users.noreply.github.com>
2022-05-04 16:46:33 +0100

    Project Vartian number on selection

--- Hooks/Modules/Documents/Collect/View/AddDocumentFieldsBefore.php
+++ Hooks/Modules/Documents/Collect/View/AddDocumentFieldsBefore.php
@@ -16,7 +16,11 @@ class AddDocumentFieldsBefore
         if (isset($_GET['project_variant'])) {
             $selected_variant = ProjectVariant::find($_GET['project_variant']);
             $selected_project = $selected_variant->project;
-            $project_variants = $selected_project->variants->pluck('name', 'id')->toArray();
+            if ($variants = $selected_project->variants) {
+                foreach ($variants as $variant) {
+                    $project_variants[$variant->id] = $variant->variant_number.' - '.$variant->name;
+                }
+            }
         }
         return view(
             'aegis::_hooks.add-document-fields-before',
