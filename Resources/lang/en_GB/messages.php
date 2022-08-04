100d1897a0bbeb58bd019224f32d081fa47d299e
Jas-n <Jas-n@users.noreply.github.com>
2022-08-04 16:26:51 +0100

    Duplicate sub-signatures from issue 2

--- Resources/lang/en_GB/messages.php
+++ Resources/lang/en_GB/messages.php
@@ -4,6 +4,9 @@ return array(
     'customer' => [
         'reference-message' => 'The Reference is a code used to identify the Customer',
     ],
+    'document' => [
+        'duplicate-approval-item-users' => 'At least one user has been selected multiple times',
+    ],
     'project' => [
         'reference-exists' => 'A project with this reference already exists.',
     ],
