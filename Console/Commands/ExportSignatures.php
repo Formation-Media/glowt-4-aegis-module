655fc779b4d60861327beb51ea279c2f867ebb83
Jas-n <Jas-n@users.noreply.github.com>
2022-12-19 12:39:09 +0000

    Signature import/export processing

--- Console/Commands/ExportSignatures.php
+++ Console/Commands/ExportSignatures.php
@@ -39,6 +39,7 @@ class ExportSignatures extends Command
      */
     public function handle()
     {
+        $base_path  = 'modules/aegis/signatures/';
         $db         = [];
         $signatures = File::where('fileable_type', User::class);
         if ($signatures->count() > 0) {
@@ -47,20 +48,36 @@ class ExportSignatures extends Command
                     $db_entry = $signature->toArray();
                     $db_entry['fileable_id']       = null;
                     $db_entry['fileable_id_email'] = $signature->fileable->email;
-                    unset($db_entry['fileable']);
-                    $db[]                          = $db_entry;
-                    $path                          = 'modules/aegis/signatures/'.str_replace('files/', '', $signature->path);
-                    $directory                     = dirname($path);
+                    $db_entry['path']              = $signature->getRawOriginal('path');
+                    unset(
+                        $db_entry['fileable'],
+                        $db_entry['id']
+                    );
+
+                    $db[]      = $db_entry;
+                    $path      = $base_path.str_replace('files/', '', $signature->path);
+                    $directory = dirname($path);
                     \Storage::makeDirectory($directory);
                     if ($signature->version === '2a') {
                         \Debug::debug();
                     } else {
+                        // Original
                         \Storage::copy($signature->path, $path);
+                        // Thumbnail
+                        \Storage::copy(
+                            str_replace('original.png', 'thumb.png', $signature->path),
+                            str_replace('original.png', 'thumb.png', $path)
+                        );
+                        // Medium
+                        \Storage::copy(
+                            str_replace('original.png', 'medium.png', $signature->path),
+                            str_replace('original.png', 'medium.png', $path)
+                        );
                     }
                 }
             }
         }
-        \Storage::put('modules/aegis/signatures/db.json', json_encode($db, JSON_PRETTY_PRINT));
-        $this->info('Finished exporting signatures to `/storage/modules/aegis/signatures/`');
+        \Storage::put($base_path.'db.json', json_encode($db, JSON_PRETTY_PRINT));
+        $this->info('Finished exporting signatures to `/storage/'.$base_path.'`');
     }
 }
